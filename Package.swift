6129bf0cca2cc2c77d0baf77cbb46952596554ac
Kamil Wyszomierski <kamil.wyszomierski@gmail.com>
2022-05-02 08:25:44 +0200

    Default Swiftlint support

--- Package.swift
+++ Package.swift
@@ -13,9 +13,20 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
+        .binaryTarget(
+            name: "SwiftLintBinary",
+            url: "https://github.com/realm/SwiftLint/releases/download/0.47.1/SwiftLintBinary-macos.artifactbundle.zip",
+            checksum: "cdc36c26225fba80efc3ac2e67c2e3c3f54937145869ea5dbcaa234e57fc3724"
+        ),
+        .plugin(
+            name: "SwiftLintPlugin",
+            capability: .buildTool(),
+            dependencies: ["SwiftLintBinary"]
+        ),
         .target(
             name: "Kukulka",
-            dependencies: []
+            dependencies: [],
+            plugins: ["SwiftLintPlugin"]
         ),
         .testTarget(
             name: "KukulkaTests",
