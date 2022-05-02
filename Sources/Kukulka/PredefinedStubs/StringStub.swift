169388f3d16635a0b195e057f84aebf2047a9bfb
Kamil Wyszomierski <kamil.wyszomierski@gmail.com>
2022-05-02 09:18:53 +0200

    Add config to swiftlint

--- Sources/Kukulka/PredefinedStubs/StringStub.swift
+++ Sources/Kukulka/PredefinedStubs/StringStub.swift
@@ -23,6 +23,7 @@ extension String.StubFactory {
 
     private static func randomString(length: Int) -> String {
         let letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
-        return String((0..<length).map { _ in letters.randomElement()! })
+        // Using force unwrap as the `nil` option never happens.
+        return String((0..<length).map { _ in letters.randomElement()! }) // swiftlint:disable:this force_unwrapping
     }
 }
