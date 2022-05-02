1ce1cd59b3035086ab5525c223340ee77114d967
Kamil Wyszomierski <kamil.wyszomierski@gmail.com>
2022-05-02 09:18:53 +0200

    Add config to swiftlint

--- Plugins/SwiftLintPlugin/plugin.swift
+++ Plugins/SwiftLintPlugin/plugin.swift
@@ -4,13 +4,15 @@ import PackagePlugin
 struct SwiftLintPlugins: BuildToolPlugin {
     
     func createBuildCommands(context: PluginContext, target: Target) async throws -> [Command] {
-        return [
+        [
             .buildCommand(
                 displayName: "Linting \(target.name)...",
                 executable: try context.tool(named: "swiftlint").path,
                 arguments: [
                     "lint",
                     "--in-process-sourcekit",
+                    "--config",
+                    "\(context.package.directory.string)/Plugins/SwiftLintPlugin/swiftlint.yml",
                     "--path",
                     target.directory.string   // only lint the files in the target directory
                 ],
